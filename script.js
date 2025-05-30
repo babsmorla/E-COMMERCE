8d838a8093f83f2334cc2f43b9859d3b9b49dad2
Umar Mohammed <umarmohammed2227@gmail.com>
2025-05-30 10:16:03 +0200

    updated js script to load from local Storage

--- script.js
+++ script.js
@@ -1,54 +1,160 @@
 
 
+const inputForm = document.getElementById("left-product-form");
+const messageContainer = document.getElementById("message");
 const container = document.getElementById("product-container");
 const baseUrl = "https://fakestoreapi.com/products";
 
-const getProduct = async () => {
+
+// fecth Data and Store in local Storage
+// Store data retrieve from API into the local storgae
+
+const fetchAndStoredData = async () => {
 
 try {
   const response = await fetch(baseUrl, {method: "GET"});
 const data = await response.json();
 
-displayData(data);
+localStorage.setItem("apiData",JSON.stringify(data));
+console.log("Data stored in localStorage", data);
+
 
 } catch (error) {
-  console.log(error);
+  console.log("Error fecthing API ",error);
 } finally {
   console.log("")
 }};
 
 
-const displayData = (data) => {
-console.log(data);
+// Function to display ONLY data from localStorage
 
-// trial side before actual display
-// create a product card
-// insert card into container div
+const loadAndDisplayLocalData = () => {
+  const dataStored = JSON.parse(localStorage.getItem("apiData"));
+
+  if (dataStored && Array.isArray(dataStored)) {
+    console.log("Data from local storage is displayed");
+    displayData(dataStored);
+
+  } else {
+    console.warn("No data in localStorage. Re-fetching");
+    fetchAndStoreData();
+  }
+};
+
+
+
+
+// Diplay Stored data after retrieving from localStorage
+
+const displayData = (data) => {
+container.innerHTML = "";
+console.log("Dipslayed Data", data);
 
 data.forEach((item, index) => {
 
   let productCard = document.createElement("div");
   productCard.classList.add("product-card");
-  productCard.innerHTML =  `<img src= ${item.image} alt = ${item.title}   />
+  productCard.innerHTML =  `<img src= ${item.image} alt = ${item.title}/>
   <h2>${item.title} </h2>
    <p>${item.description} </p>
   <p>Rating: ${item.rating.rate} </p>
-  <h3> Price: $${parseFloat (item.price)}
+  <h3>Price: $${parseFloat (item.price)}</h3>
+  <button class="btn"> Delete </button>  
   `;
+
   container.appendChild(productCard);
   
 });
 
 };
-
-getProduct();
-
-
+loadAndDisplayLocalData();
+fetchAndStoredData();
 
 
 
 
 
+// **************************************************************************************** INPUT FORM AND VALIDATION
+
+
+inputForm.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+ const image = document.getElementById("imageUrl").value;
+  const price = parseFloat(document.getElementById("price").value);
+const rate = parseFloat(document.getElementById("rate").value);
+
+  if (!title || !description || !image || !price || !rate){
+    messageContainer.style.display ="block"
+    messageContainer.innerHTML ="Please fill in all fields."
+           messageContainer.style.color="red"
+    return;
+  };
+
+
+const newProduct = {
+    title,
+    price,
+    description,
+    image,
+    category: "electronics", // add a dummy category as API expects it
+    rating: {
+      rate: rate,
+      count: 1
+    }
+  };
+
+
+  // Pull data from local storage
+
+  const newData = dataStored || [] ;
+
+  // push new product to existing
+
+  newData.push(newProduct);
+
+  // Save data back to local storage
+  localStorage.setItem("apiData",JSON.stringify(newData));
+  
+  messageContainer.style.display ="block"
+  messageContainer.innerHTML ="Product added succesfully."
+  messageContainer.style.color="green"
+
+
+ productBox.innerHTML = "";
+  productCard(container);
+
+   getProduct();
+
+    });
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+// Retrive data from localStorage after storing
+
+// const dataStored = JSON.parse(localStorage.getItem("apiData"));
+// if (dataStored) {
+//   console.log("Data retrieved from localStorage", dataStored);
+//   displayData(dataStored);
+
+// } else {
+//   console.log("Data not found");
+//   fetchAndStoredData();
+  
+// }
 
 
 
